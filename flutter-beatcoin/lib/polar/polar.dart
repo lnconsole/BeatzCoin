915d09e4b69ad4d517d81efb63f5cafef454dc7c
sebas <sebas.arriola@ibexmercado.com>
2023-05-29 09:18:19 -0600

    Work on polar controller

---
+++ flutter-beatcoin/lib/polar/polar.dart
@@ -0,0 +1,55 @@
+import 'package:get/state_manager.dart';
+import 'package:polar/polar.dart';
+
+class PolarController extends GetxController {
+  final polar = Polar();
+  late final PolarDeviceInfo selectedDevice;
+  final devices = <PolarDeviceInfo>[].obs;
+  RxInt heartRate = 0.obs;
+  RxBool isDeviceConnected = false.obs;
+
+  PolarController() {
+    polar.batteryLevel.listen((e) => print('Battery: ${e.level}'));
+    polar.deviceConnecting.listen((_) => print('Device connecting'));
+    polar.deviceConnected.listen((_) => print('Device connected'));
+    polar.deviceDisconnected.listen((_) => print('Device disconnected'));
+  }
+
+  void searchDevices() async {
+    polar.requestPermissions();
+    polar.searchForDevice().listen((e) {
+      print('Found device in scan: ${e.deviceId}');
+      devices.add(e);
+    });
+  }
+
+  void connect(String deviceId) async {
+    await polar.disconnectFromDevice(deviceId);
+    await polar.connectToDevice(deviceId);
+
+    isDeviceConnected.value = true;
+
+    await polar.sdkFeatureReady.firstWhere(
+      (e) =>
+          e.identifier == deviceId &&
+          e.feature == PolarSdkFeature.onlineStreaming,
+    );
+    final availabletypes =
+        await polar.getAvailableOnlineStreamDataTypes(deviceId);
+
+    print('available types: $availabletypes');
+
+    if (availabletypes.contains(PolarDataType.hr)) {
+      polar.startHrStreaming(deviceId).listen((e) {
+        if (e.samples.isNotEmpty) {
+          heartRate.value = e.samples[0].hr;
+        }
+      });
+    }
+  }
+
+  void disconnect(String deviceId) async {
+    await polar.disconnectFromDevice(deviceId);
+    isDeviceConnected.value = false;
+  }
+}
