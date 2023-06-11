2089f9ef22362b91d086e542fcecce5405f44cfe
cj-ibex <chongjin@ibexmercado.com>
2023-06-11 09:51:21 -0600

    listen to kind 4 and disburse sats if condition is met

--- go.mod
+++ go.mod
@@ -2,17 +2,22 @@ module github.com/lnconsole/BeatCoin
 
 go 1.20
 
+replace github.com/nbd-wtf/go-nostr v0.18.10 => github.com/lnconsole/go-nostr v0.9.1-cxl
+
 require (
+	github.com/SaveTheRbtz/generic-sync-map-go v0.0.0-20220414055132-a37292614db8
+	github.com/btcsuite/btcd v0.23.1
 	github.com/fiatjaf/go-lnurl v1.12.1
 	github.com/gin-gonic/gin v1.9.0
+	github.com/lightningnetwork/lnd v0.15.0-beta
+	github.com/nbd-wtf/go-nostr v0.18.10
 	github.com/subosito/gotenv v1.4.2
 )
 
 require (
 	github.com/aead/siphash v1.0.1 // indirect
-	github.com/btcsuite/btcd v0.23.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
-	github.com/btcsuite/btcd/btcutil v1.1.1 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
 	github.com/btcsuite/btcd/btcutil/psbt v1.1.4 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
@@ -36,13 +41,13 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kkdai/bstream v1.0.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lightninglabs/gozmq v0.0.0-20191113021534-d20a764486bf // indirect
 	github.com/lightninglabs/neutrino v0.14.2 // indirect
-	github.com/lightningnetwork/lnd v0.15.0-beta // indirect
 	github.com/lightningnetwork/lnd/clock v1.1.0 // indirect
 	github.com/lightningnetwork/lnd/queue v1.1.0 // indirect
 	github.com/lightningnetwork/lnd/ticker v1.1.0 // indirect
@@ -54,15 +59,17 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nbd-wtf/ln-decodepay v1.5.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/tidwall/gjson v1.6.1 // indirect
-	github.com/tidwall/match v1.0.1 // indirect
-	github.com/tidwall/pretty v1.0.2 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
+	github.com/valyala/fastjson v1.6.3 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/exp v0.0.0-20221106115401-f9659909a136 // indirect
 	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,56 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
-	"github.com/gin-gonic/gin"
-	"github.com/lnconsole/BeatCoin/api"
+	sm "github.com/SaveTheRbtz/generic-sync-map-go"
+	"github.com/fiatjaf/go-lnurl"
 	pmnt "github.com/lnconsole/BeatCoin/service/payment"
+	"github.com/lnconsole/BeatCoin/service/payment/lightning"
 	"github.com/lnconsole/BeatCoin/service/payment/lightning/client/ibexhub"
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip04"
 	"github.com/subosito/gotenv"
 )
+
+/*
+generate & store public key
+listen for kind4
+if secret is valid, check map[pubkey]map[day]sats > threshold
+if not, grab lightning address from profile. check if lightning address is valid
+if yes, add entry to map
+send to lightning address
+send parameterized replaceable event
+*/
+
+type BeatzcoinPayload struct {
+	Secret string `json:"beatzcoin_secret"`
+	BPM    int    `json:"bpm"`
+}
+
+type Profile struct {
+	Nip05       *string `json:"nip05"`
+	Lud16       *string `json:"lud16"`
+	Lud06       *string `json:"lud06"`
+	Website     string  `json:"website"`
+	Picture     string  `json:"picture"`
+	DisplayName string  `json:"display_name"`
+	About       string  `json:"about"`
+	Name        string  `json:"name"`
+}
+
+const (
+	thresholdRate = 180
+	rewardMsat    = 1000
+	relayUrl      = "wss://nostr-pub.wellorder.net"
+)
+
+var bcRelay *nostr.Relay
 
 func main() {
 	fmt.Println("Drop the beats, Pick up the sats")
@@ -19,19 +59,208 @@ func main() {
 		log.Printf("gotenv: %s", err)
 		return
 	}
-
 	// init ln payment service
 	if err := pmnt.InitIBEXHub(ibexhub.Credentials{
 		Email:     os.Getenv("IBEXHUB_USER_EMAIL"),
 		Password:  os.Getenv("IBEXHUB_USER_PASS"),
 		AccountID: os.Getenv("IBEXHUB_ACCOUNT_ID"),
 	}); err != nil {
-		log.Printf("Error initiating nostr service: %s", err)
+		log.Printf("Error initiating ibexhub service: %s", err)
 		return
 	}
+	// get pk
+	sk := os.Getenv("BEATZCOIN_PRIVATE_KEY")
+	pk, err := nostr.GetPublicKey(sk)
+	log.Printf("pk: %s", pk)
+	if err != nil {
+		log.Printf("Error getting pk from sk: %s", err)
+		return
+	}
+	// connect to relay
+	relay, err := nostr.RelayConnect(context.Background(), relayUrl)
+	if err != nil {
+		log.Printf("failed to connect to %s: %s", relayUrl, err)
+		return
+	}
+	bcRelay = relay
+	log.Printf("connected to: %s", bcRelay)
+	go func() {
+		for notice := range relay.Notices {
+			log.Printf("(%s) notice: %s", relay.URL, notice)
+		}
+	}()
+	// listen for kind 4
+	var (
+		t   = time.Now()
+		sub = relay.Subscribe(context.Background(), nostr.Filters{{
+			Kinds: []int{nostr.KindEncryptedDirectMessage},
+			Since: &t,
+			Tags:  nostr.TagMap{"p": []string{pk}},
+		}})
+		profilech = make(chan Profile)
+	)
+	// handle events
+	var (
+		payload BeatzcoinPayload
+	)
+	// handle kind 4
+	go func(sub *nostr.Subscription) {
+		uniqueCh := unique(sub.Events)
+		for evt := range uniqueCh {
+			// decrypt content
+			decrypted, err := decrypt(evt.PubKey, sk, evt.Content)
+			if err != nil {
+				log.Printf("Failed to decrypt: %s", err)
+				continue
+			}
+			err = json.Unmarshal([]byte(*decrypted), &payload)
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+			// validate secret
+			if os.Getenv("BEATZCOIN_SECRET") != payload.Secret {
+				log.Printf("invalid secret: %s", payload.Secret)
+				continue
+			}
+			// validate bpm
+			if payload.BPM < thresholdRate {
+				log.Printf("did not meet BPM threshold(180): %d", payload.BPM)
+				continue
+			}
+			// fetch profile
+			fetchMetadataAsync(evt.PubKey, profilech)
+		}
+	}(sub)
+	// handle profile
+	go func() {
+		for p := range profilech {
+			if p.Lud16 == nil {
+				log.Printf("client has no lud16: %s", p.Name)
+				continue
+			}
+			// validate lud16
+			_, params, err := lnurl.HandleLNURL(*p.Lud16)
+			if err != nil || params.LNURLKind() != "lnurl-pay" {
+				log.Printf("invalid lud16: %s", *p.Lud16)
+				continue
+			}
+			// send sats
+			payParams, ok := params.(lnurl.LNURLPayParams)
+			if !ok {
+				log.Printf("invalid lud16 params: %v", params)
+				continue
+			}
+			values, err := payParams.Call(int64(rewardMsat), "", nil)
+			if err != nil {
+				log.Printf("Failed to get Lightning Address values: %v", payParams)
+				continue
+			}
 
-	ginEngine := gin.Default()
-	ginEngine.POST("/api/pump", api.HandleHeartRate)
-	ginEngine.GET("/api/earnings", api.GetTxs)
-	ginEngine.Run()
+			if _, err := pmnt.MakePayment(lightning.PaymentParams{
+				Bolt11: values.PR,
+			}); err != nil {
+				log.Printf("Failed to pay Lightning Address: %s", err)
+				return
+			}
+
+			log.Printf("sending %d sats to %s", rewardMsat/1000, *p.Lud16)
+		}
+	}()
+	// wait
+	<-make(chan struct{})
+}
+
+func fetchMetadataAsync(pubkey string, ch chan Profile) {
+	go func() {
+		var (
+			sub = bcRelay.Subscribe(context.Background(), nostr.Filters{{
+				Kinds:   []int{nostr.KindSetMetadata},
+				Authors: []string{pubkey},
+			}})
+			events  []nostr.Event
+			counter int
+		)
+	LOOP:
+		for {
+			select {
+			case evt := <-sub.Events:
+				events = append(events, *evt)
+			case <-time.After(1 * time.Second):
+				if len(events) == 0 && counter == 0 {
+					// no events and this is the first second. Wait for another second
+					counter += 1
+					continue
+				} else {
+					// either we got an event, or we waited enough. Break
+					break LOOP
+				}
+			}
+		}
+
+		var latest *nostr.Event
+		for i := range events {
+			e := events[i]
+			if latest == nil ||
+				events[i].CreatedAt.After(latest.CreatedAt) {
+				latest = &e
+			}
+		}
+
+		// parse profile metadata
+		profile := Profile{}
+		if latest == nil {
+			log.Print("did not get any profile")
+			return
+		}
+
+		err := json.Unmarshal([]byte(latest.Content), &profile)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		ch <- profile
+	}()
+}
+
+func decrypt(destPubkey string, sk string, encrypted string) (*string, error) {
+	sec, err := nip04.ComputeSharedSecret(destPubkey, sk)
+	if err != nil {
+		return nil, err
+	}
+
+	decrypted, err := nip04.Decrypt(encrypted, sec)
+	if err != nil {
+		return nil, err
+	}
+
+	return &decrypted, nil
+}
+
+func unique(all chan *nostr.Event) chan nostr.Event {
+	uniqueEvents := make(chan nostr.Event)
+	emittedAlready := sm.MapOf[string, struct{}]{}
+
+	go func() {
+		for event := range all {
+			if _, ok := emittedAlready.LoadOrStore(event.ID, struct{}{}); !ok {
+				uniqueEvents <- *event
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			count := 0
+			emittedAlready.Range(func(key string, value struct{}) bool {
+				count += 1
+				return true
+			})
+			log.Printf("size of emittedAlready: %d", count)
+			time.Sleep(5 * time.Minute)
+		}
+	}()
+
+	return uniqueEvents
 }
