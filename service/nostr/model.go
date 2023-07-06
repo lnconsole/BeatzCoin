572c7185910a4b3c99a6b4060b13390b17e7a399
cj-ibex <chongjin@ibexmercado.com>
2023-07-06 17:09:36 -0600

    exercise history working

---
+++ service/nostr/model.go
@@ -0,0 +1,79 @@
+package bstr
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	sm "github.com/SaveTheRbtz/generic-sync-map-go"
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip04"
+)
+
+func Publish(ctx context.Context, evt nostr.Event, sk string, relayUrl string) (*nostr.Event, error) {
+	if evt.PubKey == "" {
+		pk, err := nostr.GetPublicKey(sk)
+		if err != nil {
+			return nil, fmt.Errorf("secretKey is invalid: %w", err)
+		}
+		evt.PubKey = pk
+	}
+
+	if evt.Sig == "" {
+		err := evt.Sign(sk)
+		if err != nil {
+			return nil, fmt.Errorf("error signing event: %w", err)
+		}
+	}
+
+	relay, err := nostr.RelayConnect(ctx, relayUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to %s: %s", relayUrl, err)
+	}
+	log.Printf("posting to: %s, %s", relayUrl, relay.Publish(ctx, evt))
+	relay.Close()
+
+	return &evt, nil
+}
+
+func Unique(all chan *nostr.Event) chan nostr.Event {
+	uniqueEvents := make(chan nostr.Event)
+	emittedAlready := sm.MapOf[string, struct{}]{}
+
+	go func() {
+		for event := range all {
+			if _, ok := emittedAlready.LoadOrStore(event.ID, struct{}{}); !ok {
+				uniqueEvents <- *event
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			count := 0
+			emittedAlready.Range(func(key string, value struct{}) bool {
+				count += 1
+				return true
+			})
+			log.Printf("size of emittedAlready: %d", count)
+			time.Sleep(5 * time.Minute)
+		}
+	}()
+
+	return uniqueEvents
+}
+
+func Decrypt(destPubkey string, sk string, encrypted string) (*string, error) {
+	sec, err := nip04.ComputeSharedSecret(destPubkey, sk)
+	if err != nil {
+		return nil, err
+	}
+
+	decrypted, err := nip04.Decrypt(encrypted, sec)
+	if err != nil {
+		return nil, err
+	}
+
+	return &decrypted, nil
+}
